aa17a9d829350aa5699fe7957e6fe9a9cee1dba7
unknown <19eucs030@skcet.ac.in>
2022-07-05 10:20:25 +0530

    catch the ball

---
+++ click.js
@@ -0,0 +1,21 @@
+var cnt=0;
+function add(){
+    
+    
+    cnt++;
+    document.getElementById("output").innerText=`You caught the ball ${cnt} times`
+}
+
+
+// var a=10;
+// for(i=0;i<5;i++){
+//     b=10;
+//     console(a+b);
+// }
+
+// var a=10;
+// function add(){
+//     var b=5;
+//     return a*b;
+// }
+// console.log(a+","+b)
